65083c1485afb752663048833c4a6c6b94bef5af
Jay Amaranayake <jayanath@gmail.com>
2020-09-05 09:12:33 +1000

    Testing Docker pw issue

--- jayanath/c05-actions02/scripts/docker_push.sh
+++ jayanath/c05-actions02/scripts/docker_push.sh
@@ -1,3 +1,4 @@
 echo "Pushing docker image to docker hub"
+echo ${DOCKER_HUB_PASSWORD}
 docker login --username ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}
 docker push ${DOCKER_HUB_USER}/3m-rocks:${SHA}
